a8ecef41663dbe9c310b9cbd4637317e5b290ae0
efrohnho@gmail.com <efrohnho@gmail.com@03257a20-16aa-5d25-8d84-16762aa0922d>
2011-04-14 03:34:09 +0000

    Getting Close

--- trunk/hadoop-computer-vision/src/edu/vt/io/Image.java
+++ trunk/hadoop-computer-vision/src/edu/vt/io/Image.java
@@ -3,6 +3,7 @@ package edu.vt.io;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -11,8 +12,10 @@ import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.WritableUtils;
 
 import com.googlecode.javacpp.BytePointer;
+import com.googlecode.javacv.cpp.opencv_core.IplImage;
 
 import static com.googlecode.javacv.cpp.opencv_core.*;
+import static com.googlecode.javacv.cpp.opencv_highgui.*;
 
 public class Image implements Writable {
 
@@ -21,33 +24,37 @@ public class Image implements Writable {
 	// IPL image
 	private IplImage image = null;
 	private WindowInfo window = null;
-	
+
+	public Image() {
+	}
+
 	// Create Image from IplImage
-	public Image(IplImage image){
+	public Image(IplImage image) {
 		this.image = image;
+		this.window = new WindowInfo();
+	}
+	
+	// Create empty Image
+	public Image(int height, int width, int depth, int nChannels){
+		this.image = cvCreateImage(cvSize(width, height), depth, nChannels);
 		this.window = new WindowInfo();
 	}
 
 	// Create Image from IplImage and IplROI
-	public Image(IplImage image, WindowInfo window){
+	public Image(IplImage image, WindowInfo window) {
 		this.image = image;
 		this.window = window;
 	}
-	
-	public IplImage getImage(){
+
+	public IplImage getImage() {
 		return image;
 	}
-	
+
 	// get window where image came from
-	public WindowInfo getWindow(){
+	public WindowInfo getWindow() {
 		return window;
 	}
-	
-	// set window image came from
-	public void setWindow(WindowInfo window){
-		this.window = window;
-	}
-	
+
 	// Pixel depth in bits
 	// PL_DEPTH_8U - Unsigned 8-bit integer
 	// IPL_DEPTH_8S - Signed 8-bit integer
@@ -56,35 +63,54 @@ public class Image implements Writable {
 	// IPL_DEPTH_32S - Signed 32-bit integer
 	// IPL_DEPTH_32F - Single-precision floating point
 	// IPL_DEPTH_64F - Double-precision floating point
-	public int getDepth(){
+	public int getDepth() {
 		return image.depth();
 	}
-	
+
 	// Number of channels.
-	public int getNumChannel(){
+	public int getNumChannel() {
 		return image.nChannels();
 	}
-	
+
 	// Image height in pixels
-	public int getHeight(){
+	public int getHeight() {
 		return image.height();
 	}
-	
+
 	// Image width in pixels
-	public int getWidth(){
+	public int getWidth() {
 		return image.width();
 	}
-	
+
 	// The size of an aligned image row, in bytes
-	public int getWidthStep(){
+	public int getWidthStep() {
 		return image.widthStep();
 	}
-	
+
 	// Image data size in bytes.
 	public int getImageSize() {
 		return image.imageSize();
 	}
 
+	// Copies one image into current image using 
+	// information contained in WindowInfo struct
+	public void insertImage(Image sourceImage){
+		IplImage img1 = this.image;
+		IplImage img2 = sourceImage.getImage();
+		WindowInfo win = sourceImage.getWindow();
+		
+		// set the ROI (location to copy into)
+		if(win.isValid()){
+			cvSetImageROI(img1, cvRect(win.getXOffset(), win.getYOffset(),sourceImage.getWidth(), sourceImage.getHeight()));
+		}
+		
+		// copy sub-image
+		cvCopy(img2, img1, null);
+		 
+		// reset the ROI
+		cvResetImageROI(img1);
+	}
+	
 	@Override
 	public void readFields(DataInput in) throws IOException {
 		// Read image information
@@ -93,16 +119,17 @@ public class Image implements Writable {
 		int depth = WritableUtils.readVInt(in);
 		int nChannels = WritableUtils.readVInt(in);
 		int imageSize = WritableUtils.readVInt(in);
-		
-		//Read window information
+
+		// Read window information
 		int windowXOffest = WritableUtils.readVInt(in);
 		int windowYOffest = WritableUtils.readVInt(in);
 		int windowHeight = WritableUtils.readVInt(in);
 		int windowWidth = WritableUtils.readVInt(in);
-		window = new WindowInfo(windowXOffest, windowYOffest, windowHeight, windowWidth);
-		
+		window = new WindowInfo(windowXOffest, windowYOffest, windowHeight,
+				windowWidth);
+
 		// Read image bytes
-		byte [] bytes = new byte[imageSize];
+		byte[] bytes = new byte[imageSize];
 		in.readFully(bytes, 0, imageSize);
 
 		image = cvCreateImage(cvSize(width, height), depth, nChannels);
@@ -117,17 +144,18 @@ public class Image implements Writable {
 		WritableUtils.writeVInt(out, image.depth());
 		WritableUtils.writeVInt(out, image.nChannels());
 		WritableUtils.writeVInt(out, image.imageSize());
-		
+
 		// Write window information
 		WritableUtils.writeVInt(out, window.getXOffset());
 		WritableUtils.writeVInt(out, window.getYOffset());
 		WritableUtils.writeVInt(out, window.getHeight());
 		WritableUtils.writeVInt(out, window.getWidth());
-		
+
 		// Write image bytes
-		byte [] bytes = image.imageData().getStringBytes();
-		int size = image.imageSize();
-		out.write(bytes, 0, image.imageSize());
+		ByteBuffer buffer = image.getByteBuffer();
+		while (buffer.hasRemaining()) {
+			out.writeByte(buffer.get());
+		}
 	}
 
 }
